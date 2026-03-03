a50769b8f02970926345de1ea7c7eb12d7449bd7
Marishel Lochan <marishel.lochan@my.uwi.edu>
2026-03-03 10:29:46 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async rewrites() {
+    const backendUrl = process.env.API_BASE_URL || "http://127.0.0.1:8080";
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL_LOCAL}/api/:path*`, // Forward to backend
+        destination: `${backendUrl}/api/:path*`,
       },
     ];
   },
