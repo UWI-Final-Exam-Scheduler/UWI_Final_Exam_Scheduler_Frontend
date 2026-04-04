2c24c5685530c10ca110b5c6d78a53c5ff5328ed
Aioblitz <60820112+Aioblitz@users.noreply.github.com>
2026-04-04 15:59:07 -0400

    Update LogoutButton.tsx

--- app/components/ui/LogoutButton.tsx
+++ app/components/ui/LogoutButton.tsx
@@ -24,10 +24,7 @@ export default function LogoutButton() {
   };
 
   return (
-    <button
-      onClick={handleLogout}
-      className="ml-auto rounded-md bg-red-600 px-4 py-2 text-white hover:bg-red-700 transition"
-    >
+    <button  onClick={handleLogout} className="ml-auto rounded-md bg-red-600 px-4 py-2 text-white hover:bg-red-700 transition">
       Logout
     </button>
   );
