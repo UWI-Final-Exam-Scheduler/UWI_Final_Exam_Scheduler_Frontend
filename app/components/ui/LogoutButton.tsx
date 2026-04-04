6ee34354eb38460bc853c4e6cdb8917a4c3725e6
Aioblitz <60820112+Aioblitz@users.noreply.github.com>
2026-04-04 16:18:06 -0400

    Update LogoutButton.tsx

--- app/components/ui/LogoutButton.tsx
+++ app/components/ui/LogoutButton.tsx
@@ -5,10 +5,15 @@ import { useRouter } from "next/navigation";
 export default function LogoutButton() {
   const router = useRouter();
 
+  const BASE_URL =
+    process.env.NODE_ENV === "development"
+      ? process.env.NEXT_PUBLIC_API_BASE_URL_LOCAL
+      : process.env.NEXT_PUBLIC_API_BASE_URL_PROD;
+
   const handleLogout = async () => {
     try {
-      const res = await fetch("/api/auth/logout", {
-        method: "POST",
+      const res = await fetch(`${BASE_URL}/api/logout`, {
+        method: "GET",
         credentials: "include",
       });
 
@@ -18,13 +23,16 @@ export default function LogoutButton() {
       } else {
         console.error("Logout failed");
       }
-    } catch (error) {
-      console.error("Logout error:", error);
+    } catch (err) {
+      console.error("Logout error:", err);
     }
   };
 
   return (
-    <button  onClick={handleLogout} className="ml-auto rounded-md bg-red-600 px-4 py-2 text-white hover:bg-red-700 transition">
+    <button
+      onClick={handleLogout}
+      className="ml-auto rounded-md bg-red-600 px-4 py-2 text-white hover:bg-red-700"
+    >
       Logout
     </button>
   );
